bdbceb5e7dce9880456ac978782541a0f297a033
Reelect <rejaealsun@gist.ac.kr>
2024-10-22 16:17:47 +0900

    fix(model): down grade version for low performance gpus

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from diffusers import DiffusionPipeline
 import torch
 
 pipe = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0",
+    "sd-legacy/stable-diffusion-v1-5",
     torch_dtype=torch.float16,
     use_safetensors=True,
     variant="fp16"
